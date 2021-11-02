30c3dd788d62cdda91682e85e79c6cb3442e2857
chanstar9 <46039135+chanstar9@users.noreply.github.com>
2021-11-02 17:59:28 +0900

    Add files via upload

---
+++ bs_formula.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: Chankyu Choi
+:Date: 2019. 09. 15
+"""
+from scipy.stats import norm
+import numpy as np
+
+
+def d_1(s, k, tow, r, q, vol):
+    return (np.log(s / k) + (r - q + (vol ** 2) * 0.5) * tow) / (vol * np.sqrt(tow))
+
+
+def bs_price(s, k, tow, r, q, vol, position, cp=1):
+    d1 = d_1(s, k, tow, r, q, vol)
+    d2 = d1 - vol * np.sqrt(tow)
+    return position * (cp * s * np.exp(-q * tow) * norm.cdf(cp * d1) - cp * k * np.exp(-r * tow) * norm.cdf(cp * d2))
+
+
+def bs_delta(s, k, tow, r, q, vol, position, cp=1):
+    d1 = d_1(s, k, tow, r, q, vol)
+    return position * (cp * np.exp(-q * tow) * norm.cdf(cp * d1))
+
+
+def bs_vega(s, k, tow, r, q, vol, position):
+    d1 = d_1(s, k, tow, r, q, vol)
+    return position * (s * np.exp(-q * tow) * np.sqrt(tow) * norm.pdf(d1))
+
+
+def bs_gamma(s, k, tow, r, q, vol, position):
+    d1 = d_1(s, k, tow, r, q, vol)
+    return position * (np.exp(-q * tow) * norm.pdf(d1) / (s * vol * np.sqrt(tow)))
+
+
+def bs_theta(s, k, tow, r, q, vol, position, cp=1):
+    d1 = d_1(s, k, tow, r, q, vol)
+    d2 = d1 - vol * np.sqrt(tow)
+    return position * (-np.exp(-q * tow) * s * norm.pdf(d1) * vol / (2 * np.sqrt(tow)) - cp * r * k * np.exp(
+        -r * tow) * norm.cdf(cp * d2) + cp * q * s * np.exp(-q * tow) * norm.cdf(cp * d1))
+
+
+def bs_rho(s, k, tow, r, q, vol, position, cp=1):
+    d1 = d_1(s, k, tow, r, q, vol)
+    d2 = d1 - vol * np.sqrt(tow)
+    return position * (cp * k * tow * np.exp(-r * tow) * norm.cdf(cp * d2))
+
+
+def bs_vanna(s, k, tow, r, q, vol, position):
+    d1 = d_1(s, k, tow, r, q, vol)
+    return position * (np.sqrt(tow) * norm.pdf(d1) * (1 - d1))
+
+
+def IV(s, k, tow, r, q, mkt_price, cp=1):
+    global price
+    vol = 0.15
+    price = bs_price(s, k, tow, r, q, vol, cp)
+    while abs(price - mkt_price) >= 1.0e-6:
+        price = bs_price(s, k, tow, r, q, vol, cp)
+        vol = vol - (price - mkt_price) / bs_vega(s, k, tow, r, q, vol, 1)
+    return vol
---
+++ columns.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: Chankyu Choi
+:Date: 2021. 08. 13
+"""
+
+# index ticker
+SNP = '^SPX'
+NASDAQ = '^NDX'
+
+# stock ticker
+AAPL = 'AAPL'
+TSLA = 'TSLA'
+PLTR = 'PLTR'
+
+# coin ticker
+BTCUSDT = 'BTCUSDT'
+BTC = 'BTC'
+ETHUSDT = 'ETHUSDT'
+ETH = 'ETH'
+XRPUSDT = 'XRPUSDT'
+BNBUSDT = 'BNBUSDT'
+ADAUSDT = 'ADAUSDT'
+SOLUSDT = 'SOLUSDT'
+FTTUSDT = 'FTTUSDT'
+AXSUSDT = 'AXSUSDT'
+LUNAUSDT = 'LUNAUSDT'
+RAYUSDT = 'RAYUSDT'
+
+Binance_ticker_list = [BTCUSDT, ETHUSDT, SOLUSDT, XRPUSDT, BNBUSDT, ADAUSDT, FTTUSDT, AXSUSDT, LUNAUSDT, RAYUSDT]
+
+# etc
+ID = '_id'
+NUM = 'Number'
+CALENDAR = 'Calendar'
+YEAR = 'Year'
+MONTH = 'Month'
+DAY = 'Day'
+HOUR = 'Hour'
+MINUTE = 'Minute'
+TIMESTAMP = 'Timestamp'
+
+UNDERLYING = 'Underlying'
+MAT = 'Maturity'
+TTM = 'Time_to_Maturity'
+VOL = 'volatility'
+T_VOL = 'Trade_volume'
+OI = 'Open_interest'
+DIR = 'Direction'
+CP = 'callput'
+NAME = 'Name'
+LAST_P = 'Last_price'
+UNDER_P = 'Underlying_price'
+INSTRUMENT_NAME = 'instrument_name'
+STRIKE = 'Strike'
+MONEYNESS = 'Moneyness'
+
+# 호가
+BID_1P = '1st_bid_price'
+BID_1A = '1st_bid_amount'
+BID_2P = '2st_bid_price'
+BID_2A = '2st_bid_amount'
+BID_3P = '3st_bid_price'
+BID_3A = '3st_bid_amount'
+ASK_1P = '1st_ask_price'
+ASK_1A = '1st_ask_amount'
+ASK_2P = '2st_ask_price'
+ASK_2A = '2st_ask_amount'
+ASK_3P = '3st_ask_price'
+ASK_3A = '3st_ask_amount'
+
+# calendar
+JAN = 'JAN'
+FEB = 'FEB'
+MAR = 'MAR'
+APR = 'APR'
+MAY = 'MAY'
+JUN = 'JUN'
+JUL = 'JUL'
+AUG = 'AUG'
+SEP = 'SEP'
+OCT = 'OCT'
+NOV = 'NOV'
+DEC = 'DEC'
